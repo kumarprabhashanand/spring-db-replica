8fd0b814b72a6400537845bb4c8fd9ddc85bda96
Prabhash <kumarprabhash1995@gmail.com>
2024-04-21 12:14:38 +0200

    initial code

---
+++ src/main/java/com/prabhash/test/SpringDbReplicaTestApplication.java
@@ -0,0 +1,13 @@
+package com.prabhash.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbReplicaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDbReplicaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prabhash/test/SpringDbReplicaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prabhash.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDbReplicaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
